6eb31b669fe86e0756648c1d284b9e1d56872de7
Niki van Driel <NikiD@zorgenzekerheid.nl>
2022-08-20 16:38:53 +0200

    feat(calculator): fix calculation

--- main.js
+++ main.js
@@ -1,22 +1,76 @@
-const myInput = document.getElementById('result');
-const myOperator = document.getElementById('operator');
+const calculator = document.querySelector('#calculator'); 
+const myInput = calculator.querySelector('#keys');
+const display = calculator.querySelector('#result');
+const operatorDisplay = calculator.querySelector('#operator');
 
-function onClickNumber(clickedNumber) {
-    console.log('a number is clicked', clickedNumber);
-    myInput.value += clickedNumber; 
-};
+myInput.addEventListener('click', event => {
+    if (!event.target.closest('button')) return
 
-function onClickOperator(clickedOperator) {
-    console.log('operator is clicked', clickedOperator);
-    myOperator.value = clickedOperator;
+    const key = event.target;
+    const keyValue = key.textContent;
+    const displayValue = display.placeholder;
+    const type = key.dataset.type;
+
+    // if it is a number key
+    if (type ==='number') {
+        if (displayValue === '0') {
+            display.placeholder = keyValue;
+        } 
+        // check if last input and current input is a dot  
+        else if(displayValue.includes('.') && keyValue === '.') {
+         return;   
+        } 
+        else if(calculator.dataset.previousKeyType === 'operator') {
+            display.placeholder = keyValue;
+        }
+        else {
+            display.placeholder = displayValue + keyValue;
+        }
+    }
+
+    // if it is a operator key
+    if (type === 'operator') {
+        const currentActiveOperator = calculator.querySelector('[data-state="selected"]');
+        if(currentActiveOperator) {
+            currentActiveOperator.dataset.state = ''
+        }
+        key.dataset.state = 'selected';
+        operatorDisplay.placeholder = keyValue,
+        
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = keyValue;
+    };
+    
+    // if it is cancel
+    if(type === 'cancel') {
+        display.placeholder = '0';
+        clearOperator()
+    }
+    
+    // if it is equal
+    if(type === 'equal') {
+        // perform a calculation
+        const firstNumber = calculator.dataset.firstNumber;  
+        const secondNumber = displayValue;
+        const operator = calculator.dataset.operator;
+        display.placeholder = calculate(firstNumber, operator, secondNumber);
+        clearOperator()
+
+    }
+    calculator.dataset.previousKeyType = type;
+})
+
+function calculate (firstNumber, operator, secondNumber) {
+
+    let result = '';
+    if(operator === '+') result = Number(firstNumber) + Number(secondNumber);
+    if(operator === '-') result = Number(firstNumber) - Number(secondNumber);
+    if(operator === 'x') result = Number(firstNumber) * Number(secondNumber);
+    if(operator === '/') result = Number(firstNumber) / Number(secondNumber);
+
+    return result.toFixed(10);
 }
 
-function onClickCancel(clickedCancel) {
-    console.log('cancel is clicked', clickedCancel);
-    myInput.value = ' ';
+function clearOperator() {
+    operatorDisplay.placeholder = '';
 }
-
-function onClickEquals(clickedEquals) {
-    console.log('equals is clicked', clickedEquals);
-}
-
